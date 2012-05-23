f36e036e1c39472a54626f28e1502eabe19454f3
Michal Simek <michal.simek@petalogix.com>
2012-05-23 09:58:59 +0200

    Revert "bsp:uboot:remove debug message"
    This reverts commit a19b244eaa50bb0e929d8e728144255067fc4f46.
    
    ps7_uart multi can't behave as uartlite. It is separate driver.

--- data/uboot_v2_1_0.tcl
+++ data/uboot_v2_1_0.tcl
@@ -456,7 +456,9 @@ proc uboot_intc {os_handle proc_handle config_file config_file2 freq system_bus}
 					}
 					incr uart16550_count
 				}
-				"ps7_uart" -
+				"ps7_uart" {
+					puts "============= multi uart?"
+				}
 				"opb_uartlite" -
 				"xps_uartlite" -
 				"axi_uartlite" -
